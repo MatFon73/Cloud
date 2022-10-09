8369d7a90306c394f2e969e66f3a7a8d02803125
Mateo Fonseca <mateofon78@hotmail.com>
2022-10-09 15:17:05 -0500

    error porcentage storage

--- php/Storage_controller.php
+++ php/Storage_controller.php
@@ -5,11 +5,11 @@ class Storage
     {
         $Storage = round((disk_total_space($Url) / 1073741824), 2);
         $Free = round((disk_free_space($Url) / 1073741824), 2);
-        $percentage = round((($Free / $Storage) * 100), 2) . "%";
+        $percentage =  round((($Free / $Storage) * 100), 2) ;
         try {
             echo "<div id='process-back'>";
-            echo "<h5 style='left:25%; position:absolute' title='" . $percentage . ' Free' . "'>" . $percentage . ' Free' . "</h5>";
-            echo "<div style='width:" . $percentage . "' id='process'>";
+            echo "<h5 style='left:25%; position:absolute' title='" . $percentage . ' Free' . "'>" . $percentage . '% Free' . "</h5>";
+            echo "<div style='width:" . (100 - $percentage). "%' id='process'>";
             echo "<h5>&nbsp;</h5></div></div>";
             echo "<h5 style='color:white;' class='text-center'>" . $Free . " GB  Free / " . $Storage . " GB " . "</h5>";
         } catch (Exception $e) {
